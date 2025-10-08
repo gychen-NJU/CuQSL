77e3e14fa607f912019a870debae43926d793a51
陈国银 <106894282+gychen-NJU@users.noreply.github.com>
2025-10-08 21:24:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 setup(
@@ -16,22 +15,4 @@ setup(
     author='Chen Guoyin',
     author_email='gychen@smail.nju.edu.cn',
     description='Compute the QSL for a given point data based on CUDA',
-=======
-from setuptools import setup, find_packages
-
-setup(
-    name='cuQSL',
-    version='1.0.0',
-    packages=find_packages(where="src"),  # 自动查找 src 下的包
-    package_dir={"": "src"},              # 包根目录映射到 src
-    include_package_data=True,
-    install_requires=[
-        'numpy>=1.18.5',
-        'torch>=1.10.0',
-        'matplotlib>=3.2.2',
-    ],
-    author='Chen Guoyin',
-    author_email='gychen@smail.nju.edu.cn',
-    description='Compute the QSL for a given point data based on CUDA',
->>>>>>> d905b53dad074c2669079616db069ce6e0301523
 )
