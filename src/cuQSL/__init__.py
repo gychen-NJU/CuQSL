ac8c0800ac9c474ccf32b5d6b47dbf501943c818
陈国银 <106894282+gychen-NJU@users.noreply.github.com>
2025-10-08 21:26:16 +0800

    Update __init__.py

--- src/cuQSL/__init__.py
+++ src/cuQSL/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 cuQSL package initialization module
@@ -12,17 +11,3 @@ from .cuQSL_cat import qsl_solver_cat as cat
 from .cuQSL_sph import qsl_solver_sph as sph
 
 __all__ = ['cat', 'sph', "__version__"]
-=======
-# -*- coding: utf-8 -*-
-"""
-cuQSL package initialization module
-"""
-
-__author__ = 'Chen Guoyin'
-__email__ = 'gychen@smail.nju.edu.cn'
-__version__ = '1.0.0'
-
-from .cuQSL_cat import qsl_solver_cat as cat
-
-__all__ = ['cat', "__version__"]
->>>>>>> d905b53dad074c2669079616db069ce6e0301523
